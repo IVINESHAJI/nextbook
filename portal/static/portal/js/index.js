ead5f9e695aa5588ce789d529e59973c579d00b3
Vijay Varadarajan <vijayvaradarajan84@gmail.com>
2023-12-30 13:50:06 +0530

    added typewriter script

---
+++ portal/static/portal/js/index.js
@@ -0,0 +1,7 @@
+const hero = new Typed('.changing-hero', {
+    strings: ['Find books you need.', 'Share books you liked.', 'Discover books you\'d love.', 'Connect, Help and Inspire.'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 500,
+    loop: true
+});
